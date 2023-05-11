8a0b3a2aeb1c1c92c9c1b5307ac62d10e987ae1d
Suman <35394894+sumangeo@users.noreply.github.com>
2023-05-11 16:05:11 +0600

    Update _document.js
    from guideline here: https://dev.to/siddharthroy/build-a-free-blog-with-nextjs-and-netlify-cms-125h

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          {/* Netlify Widget */}
+          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
-        >
+		  >
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
